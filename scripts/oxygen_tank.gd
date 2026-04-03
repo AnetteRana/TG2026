1208be3dc48b5a779fb51d273cf7e92dd24428a7
Anette Rana <pertrosfoliea@gmail.com>
2026-04-03 14:19:49 +0200

    aircan audio innit

--- scripts/oxygen_tank.gd
+++ scripts/oxygen_tank.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-var bubble_effect : GPUParticles2D
+@onready var bubble_effect : GPUParticles2D
+@onready var audio = $AudioStreamPlayer2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	bubble_effect = $tanks_ParticleBubbles
-
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,4 +16,5 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		bubble_effect.restart()
 		$Sprite2D.frame = 1
+		audio.play()
 	
